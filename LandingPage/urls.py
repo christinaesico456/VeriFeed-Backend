af31b41e50bf33afd83971723dd64cb22573f661
Christina Esico <christinaesico456@gmail.com>
2025-11-19 20:03:12 +0800

    Try again for default image

--- LandingPage/urls.py
+++ LandingPage/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .debug_views import check_media_files
+from django.views.static import serve
+
 
 
 router = DefaultRouter()
@@ -16,6 +18,9 @@ urlpatterns = [
     path('api/', include('reviews.urls')), 
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+
 ]
 
 # Serve media files (temporary solution for Railway)
