e9ebf4da5767baf7f1d3944d7c6a7e876c7aba06
Christina Esico <christinaesico456@gmail.com>
2025-11-20 07:11:50 +0800

    Fix again config

--- LandingPage/settings.py
+++ LandingPage/settings.py
@@ -141,16 +141,16 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 5 * 1024 * 1024
 
 # EMAIL CONFIGURATION
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_HOST = (os.getenv("EMAIL_HOST", "smtp.gmail.com"))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_USE_SSL = False  
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER", "verifeedofficial@gmail.com")
+SERVER_EMAIL = os.getenv("EMAIL_HOST_USER", "verifeedofficial@gmail.com")
 EMAIL_USE_LOCALTIME = False
-EMAIL_TIMEOUT = 10
+EMAIL_TIMEOUT = 30
 
 # SECURITY SETTINGS
 if not DEBUG:
