f37fd446782a2df82449972d41724f2da0ab993f
Christina Esico <christinaesico456@gmail.com>
2025-11-19 19:06:58 +0800

    Fix Default Image Error

--- LandingPage/urls.py
+++ LandingPage/urls.py
@@ -15,7 +15,9 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
-# Serve media files in development
+# Serve media files (temporary solution for Railway)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files in development only
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
