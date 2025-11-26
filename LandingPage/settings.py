7ffbaadfa09edc7f12eca84f2483c57b8af3acee
Christina Esico <christinaesico456@gmail.com>
2025-11-26 17:16:17 +0800

    Fix email

--- LandingPage/settings.py
+++ LandingPage/settings.py
@@ -151,11 +151,11 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = (os.getenv("EMAIL_HOST", "smtp.sendgrid.net"))
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "apikey")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False  
 DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER", "verifeedofficial@gmail.com")
-SERVER_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "verifeedofficial@gmail.com")
+SERVER_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "verifeed@sendgrid.net")
 EMAIL_USE_LOCALTIME = False
 EMAIL_TIMEOUT = 60
 
